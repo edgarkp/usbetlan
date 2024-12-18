89b8ae9862474cbd60df4a3b74cb46a183f52046
unknown <edgar.kouajiep@gmail.com>
2024-12-19 00:50:14 +0100

    feat: finalize generate data

--- src/generate_data.py
+++ src/generate_data.py
@@ -25,10 +25,14 @@ Generate data function will create the data from the 7th october to 20th decembe
 # create a function that will take as input the datetime and identify which trigger is necessary and update the portfolio accordingly
 from src.update_portfolio import update_portfolio
 import os
+from datetime import date, timedelta
+
 
 def get_input_update_portofolio(date):
-    is_weekly = date.utcnow().weekday() == 0  # Monday
-    is_monthly = date.utcnow().day == 1       # First day of the month
+    print(date)
+    print(date.utcnow())
+    is_weekly = date.weekday() == 0  # Monday
+    is_monthly = date.day == 1       # First day of the month
 
     trig_update_weights_1 = False # Always false
     trig_update_weights_2 = True   # Always true
@@ -51,3 +55,18 @@ def update_portfolios(date):
                          os.getenv['DB_NAME'])
 
 # use the above function to loop all the datetimes from the 8th october to 20th december
+start = date(2024, 10, 8)
+end = date(2024, 12, 20)
+
+# get list of all days
+all_days = [start + timedelta(x + 1) for x in range((end - start).days)]
+
+# filter business days
+# weekday from 0 to 4. 0 is monday adn 4 is friday
+# increase counter in each iteration if it is a weekday
+count = sum(1 for day in all_days if day.weekday() < 5)
+print('Number of business days is:', count)
+
+for day in all_days:
+    if day.weekday() < 5:
+        update_portfolios(day)
